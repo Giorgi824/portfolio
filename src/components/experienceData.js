22c19e6ebc8ae48176b94191ac69fd2b29cd95cf
Giorgi Gonadze <gonadze.giorgi1@gmail.com>
2023-10-10 21:13:08 +0400

    removed extra stuff

--- src/components/experienceData.js
+++ src/components/experienceData.js
@@ -8,26 +8,56 @@ import tailwindLogo from "../../public/assets/logos/tailwind.png";
 import jqueryLogo from "../../public/assets/logos/jquery.png";
 import bootstrapLogo from "../../public/assets/logos/bootstrap.png";
 export const techs = [
-  { id: 1, src: htmlLogo, title: "HTML", style: "shadow-[#e34e26]" },
-  { id: 2, src: cssLogo, title: "CSS", style: "shadow-[#1b73ba]" },
   {
-    id: 3,
+    id: crypto.randomUUID(),
+    src: htmlLogo,
+    title: "HTML",
+    style: "shadow-[#e34e26]",
+  },
+  {
+    id: crypto.randomUUID(),
+    src: cssLogo,
+    title: "CSS",
+    style: "shadow-[#1b73ba]",
+  },
+  {
+    id: crypto.randomUUID(),
     src: javaScriptLogo,
     title: "JavaScript",
     style: "shadow-[#f7df1e]",
   },
-  { id: 4, src: reactLogo, title: "React", style: "shadow-[#53c1de]" },
   {
-    id: 5,
-    src: typeScriptLogo,
-    title: "TypeScript",
-    style: "shadow-[#007acc]",
+    id: crypto.randomUUID(),
+    src: reactLogo,
+    title: "React",
+    style: "shadow-[#53c1de]",
   },
-  { id: 6, src: gitLogo, title: "GitHub", style: "shadow-[#dde0e4]" },
-  { id: 7, src: tailwindLogo, title: "Tailwind", style: "shadow-[#16bcb8]" },
-  { id: 8, src: jqueryLogo, title: "jQuery", style: "shadow-[#1266a9]" },
+  // {
+  //   id: crypto.randomUUID(),
+  //   src: typeScriptLogo,
+  //   title: "TypeScript",
+  //   style: "shadow-[#007acc]",
+  // },
   {
-    id: 9,
+    id: crypto.randomUUID(),
+    src: gitLogo,
+    title: "GitHub",
+    style: "shadow-[#dde0e4]",
+  },
+  {
+    id: crypto.randomUUID(),
+    src: tailwindLogo,
+    title: "Tailwind",
+    style: "shadow-[#16bcb8]",
+  },
+  {
+    id: crypto.randomUUID(),
+    src: jqueryLogo,
+    title: "jQuery",
+    style: "shadow-[#1266a9]",
+  },
+  {
+    id: crypto.randomUUID(),
     src: bootstrapLogo,
     title: "Bootstrap",
     style: "shadow-[#543b7a]",
